1b49dc87bace2b2169ce4c00160ca777a77c4e48
Lucas Park <solance@email.unc.edu>
2021-09-15 20:46:05 -0400

    Funny codes

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,24 @@
 """Finding duplicate letters in a word."""
 
 __author__ = "123456789"
+
+word: str = input("Enter a word: ")
+
+dupe: bool = False
+
+i: int = 0
+
+j: int = 1
+
+while i < len(word):
+    while j < len(word):
+        if word[i] == word[j]:
+            dupe: bool = True
+            j += 1
+        else:
+            j += 1
+    else:
+        i += 1
+        j = i + 1
+
+print("Found duplicate: " + str(dupe))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,3 +4,17 @@ __author__ = "123456789"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+depth = int(input("Depth: "))
+
+i: int = 0
+
+trees: str = ("")
+
+if depth <= 0:
+    print()
+else:
+    while depth > i:
+        trees = trees + TREE
+        print(trees)
+        i += 1
