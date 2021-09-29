2403bf6a92226326eda35307f46de0dafe581ae1
Lucas Park <solance@email.unc.edu>
2021-09-28 20:46:40 -0400

    brobro 2

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,17 +4,22 @@ __author__ = "730408456"
 
 
 def all(test: list[int], x: int) -> bool:
+    """Test to see if every number in a list matches the input provided."""
     i = 0
-    while len(test) > i:
-        if test[i] == x:
-            i += 1
-        else:
-            return False
+    if len(test) == 0:
+        return False
     else:
-        return True
+        while len(test) > i:
+            if test[i] == x:
+                i += 1
+            else:
+                return False
+        else:
+            return True
 
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Test to see if two lists are the same."""
     i = 0
     if len(list1) != len(list2):
         return False
@@ -29,6 +34,7 @@ def is_equal(list1: list[int], list2: list[int]) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Tests to find the largest number in a list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     else:
