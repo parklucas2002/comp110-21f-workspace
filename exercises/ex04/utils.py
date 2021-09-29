da1f5284904b5da0934639de6902c300dbc45e62
Lucas Park <solance@email.unc.edu>
2021-09-28 20:42:21 -0400

    brobro

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,39 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730408456"
 
 
-# TODO: Implement your functions here.
+def all(test: list[int], x: int) -> bool:
+    i = 0
+    while len(test) > i:
+        if test[i] == x:
+            i += 1
+        else:
+            return False
+    else:
+        return True
+
+
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    i = 0
+    if len(list1) != len(list2):
+        return False
+    else:
+        while i < len(list1) and len(list2):
+            if list1[i] == list2[i]:
+                i += 1
+            else:
+                return False
+        else:
+            return True
+
+
+def max(input: list[int]) -> int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        most = input[0]
+        for x in input:
+            if x > most:
+                most = x
+        return most
