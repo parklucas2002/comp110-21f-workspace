8d57a5e0a0d1c41215be89b75b54e36add928bde
Lucas Park <solance@email.unc.edu>
2021-10-06 19:52:27 -0400

    stink

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,38 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730408456"
 
-# Define your functions below
+
+def only_evens(evens: list[int]) -> list[int]:
+    """Returns a list with only even elements."""
+    x: int = 0
+    while x < len(evens):
+        if evens[x] % 2 == 0:
+            x += 1
+        else:
+            evens.pop(x)
+    return evens
+
+
+def sub(a_list: list[int], x: int, y: int) -> list[int]:
+    """Generate a subset of another list."""
+    b_list: list[int] = list()
+    while x < y:
+        b_list.append(a_list[x])
+        x += 1
+    else:
+        return b_list
+
+
+def concat(c_list: list[int], d_list: list[int]) -> list[int]:
+    """When given two lists, concat them."""
+    e_list: list[int] = list()
+    x = 0
+    a = 0
+    while x < len(c_list):
+        e_list.append(c_list[x])
+        x += 1
+    while a < len(d_list):
+        e_list.append(d_list[a])
+        a += 1
+    return e_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,59 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730408456"
+
+
+def test_only_evens() -> None:
+    evens: list[int] = [1, 2, 3]
+    assert only_evens(evens) == [2]
+
+
+def test_only_evens_two() -> None:
+    evens: list[int] = [2, 5, 6, 10, 13, 14]
+    assert only_evens(evens) == [2, 6, 10, 14]
+
+
+def test_only_evens_three() -> None:
+    evens: list[int] = [1, 3, 5, 7]
+    assert only_evens(evens) == []
+
+
+def test_sub() -> None:
+    a_list: list[int] = [10, 15, 20, 25, 30]
+    x: int = 1
+    y: int = 4
+    assert sub(a_list, x, y) == [15, 20, 25]
+
+
+def test_sub_two() -> None:
+    a_list: list[int] = [0, 1, 3]
+    x: int = 1
+    y: int = 3
+    assert sub(a_list, x, y) == [1, 3]
+
+
+def test_sub_three() -> None:
+    a_list: list[int] = [1, 2]
+    x: int = 1
+    y: int = 2
+    assert sub(a_list, x, y) == [2]
+
+
+def test_concat() -> None:
+    c_list: list[int] = [1, 2, 3]
+    d_list: list[int] = [4, 5, 6]
+    assert concat(c_list, d_list) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_two() -> None:
+    c_list: list[int] = [2, 4]
+    d_list: list[int] = [10, 13, 15]
+    assert concat(c_list, d_list) == [2, 4, 10, 13, 15]
+
+
+def test_concat_three() -> None:
+    c_list: list[int] = []
+    d_list: list[int] = [110, 111]
+    assert concat(c_list, d_list) == [110, 111]
