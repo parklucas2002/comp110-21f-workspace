5f7dfe45f92bb036df2740d57e05c89c873eadb9
Lucas Park <solance@email.unc.edu>
2021-10-20 22:05:10 -0400

    shut up

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,46 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730408456"
 
-# Define your functions below
+
+def invert(x: dict[str, str]) -> dict[str, str]:
+    """Inverts a dictionary."""
+    inverted: dict[str, str] = {}
+    keys = []
+    for z in x:
+        key: str = x[z]
+        for m in keys:
+            if key == m:
+                raise KeyError("Duplicate keys")
+        keys.append(key)
+        inverted[key] = key
+    print(inverted)
+    return inverted
+
+
+def favorite_color(c: dict[str, str]) -> str:
+    """Returns the most common value of a dictionary."""
+    new: dict[str, int] = {}
+    mostfrequent: str = ""
+    a: int = 0
+    for i in c:
+        if c[i] in new:
+            new[c[i]] += 1
+        else:
+            new[c[i]] = 1
+    for i in new:
+        if new[i] > a:
+            mostfrequent = i
+            a = new[i]
+    return mostfrequent
+
+
+def count(a: list[str]) -> dict[str, int]:
+    """Returns a dictionary that records how often a value appeared in an original list."""
+    b: dict[str, int] = {}
+    for i in a:
+        if i in b:
+            b[i] += 1
+        else:
+            b[i] = 1
+    return b
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,59 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730408456"
+
+
+def test_invert_one() -> None:
+    """Test invert 1."""
+    x = {'b': 'c', 'a': 'd', 'c': 'a'}
+    assert invert(x) == {'c': 'c', 'd': 'd', 'a': 'a'}
+
+
+def test_invert_two() -> None:
+    """Test invert 2."""
+    x = {'apple': '1', 'banana': '2'}
+    assert invert(x) == {'1': '1', '2': '2'}
+
+
+def test_invert_three() -> None:
+    """Test invert 3."""
+    x = {'1': '1', '2': '2'}
+    assert invert(x) == {'1': '1', '2': '2'}
+
+
+def test_favorite_color_one() -> None:
+    """Test favorite_color 1."""
+    c = {'Lucas': 'blue', 'Owen': 'blue', 'George': 'red'}
+    assert favorite_color(c) == "blue"
+
+
+def test_favorite_color_two() -> None:
+    """Test favorite_color 2."""
+    c = {'Lucas': 'orange', 'Owen': 'blue', 'George': 'red'}
+    assert favorite_color(c) == "orange"
+
+
+def test_favorite_color_three() -> None:
+    """Test favorite_color 3."""
+    c = {'Lucas': 'blue', 'Owen': 'red', 'George': 'red'}
+    assert favorite_color(c) == "red"
+
+
+def test_count_one() -> None:
+    """Test count 1."""
+    a = ['1', '2', '1', '2', '1', '1', '3']
+    assert count(a) == {'1': 4, '2': 2, '3': 1}
+
+
+def test_count_two() -> None:
+    """Test count 1."""
+    a = ['1', '1', '1', '1', '1', '1', '1']
+    assert count(a) == {'1': 7}
+
+
+def test_count_three() -> None:
+    """Test count 3."""
+    a = ['apple', 'orange', 'orange', 'apple', 'apple', 'banana', 'orange']
+    assert count(a) == {'apple': 3, 'orange': 3, 'banana': 1}
